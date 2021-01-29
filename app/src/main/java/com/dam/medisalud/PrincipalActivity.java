4e42a9b8792d7f163934deaeee1ad4bd9e9b6478
Piero Villanueva <pierovillanueva.g@hotmail.com>
2021-01-29 14:26:58 -0500

    Pelado Rctmr

--- app/src/main/java/com/dam/medisalud/PrincipalActivity.java
+++ app/src/main/java/com/dam/medisalud/PrincipalActivity.java
@@ -1,8 +1,10 @@
 package com.dam.medisalud;
 
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,10 +20,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -93,27 +97,43 @@ public class PrincipalActivity extends Fragment {
 
         adapter = new MedicamentoAdapter(medicamentoList) ;
         recycler.setAdapter(adapter);
-        database.getReference().getRoot().addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                medicamentoList.removeAll(medicamentoList);
-                for (DataSnapshot Snapshot:dataSnapshot.getChildren()
-                     ) {
-                    Medicamento medicam = Snapshot.getValue(Medicamento.class);
-                    medicamentoList.add(medicam);
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String fecha = dayOfMonth + "/" + (month+1) +"/" + year;
+                String fecha = (dayOfMonth + "/" + "0"+(month+1) +"/" + year).toString();
+                Query q=database.getReference("Medicamentos").orderByChild("fecha").equalTo(fecha);
+                q.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                        medicamentoList.removeAll(medicamentoList);
+
+                            Medicamento medicam = snapshot.getValue(Medicamento.class);
+                            System.out.println(medicam.getPastilla());
+                            medicamentoList.add(medicam);
+                        adapter.notifyDataSetChanged();
+                    }
+
+0
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
             }
         });
